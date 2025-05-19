4a57728e408cfefff41807eb92a2e9ac910ea16a
LimLims <4466362+LimLims@users.noreply.github.com>
2025-05-19 18:49:10 +1000

    .

--- core/finetuning.py
+++ core/finetuning.py
@@ -440,7 +440,7 @@ def run_dpo_finetune(config: dict, experiment_run_dir: Path):
     
     # Hard-disable gradient-checkpointing for TDPO
     if mode in ["tdpo", "tdpo-multi"]:
-        model.config._attn_implementation = "flash_attention_2"
+        #model.config._attn_implementation = "flash_attention_2"
         # turn off every ckpt flag Unsloth uses
         if hasattr(model, "gradient_checkpointing_disable"):
             model.gradient_checkpointing_disable()
@@ -480,7 +480,7 @@ def run_dpo_finetune(config: dict, experiment_run_dir: Path):
         )
 
     if mode in ["tdpo", "tdpo-multi"]:
-        model.config._attn_implementation = "flash_attention_2"
+        #model.config._attn_implementation = "flash_attention_2"
         # turn off every ckpt flag Unsloth uses
         if hasattr(model, "gradient_checkpointing_disable"):
             model.gradient_checkpointing_disable()
